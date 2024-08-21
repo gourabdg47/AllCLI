ed864b1b27c34eb92078a21122eab0bdfa084834
gourabdg47 <gourabdasgupta26994@gmail.com>
2024-08-21 14:36:54 +0530

    few more menu options

--- configs/config.py
+++ configs/config.py
@@ -5,4 +5,5 @@ LOG_DIRECTORY = 'statics/logs'
 JOURNAL_DIRECTORY = 'statics/files/journals'
 MISSION_DIR = "statics/files/mission_logs"
 
+# Template path
 MISSION_LOG_TEMPLATE_PATH = "templates/mission_jinja/mission_log_template.jinja"
